9bae9bc11a22d037e6f77b0879f094ca7def866e
Rishi Bhatia <RishiBhatia81@gmail.com>
2019-06-30 22:44:38 -0400

    first commit

---
+++ blackjack.js
@@ -0,0 +1,74 @@
+console.log("hello ");
+
+function player1Hand() {
+var countplayer1=Math.floor(Math.random()*10+1);
+var countplayer2=Math.floor(Math.random()*10+1);
+var total1=countplayer1+countplayer2
+console.log("Player one has" + total1);
+return total1
+}
+
+/*
+
+//function player2Hand() {
+var countplayer21=Math.floor(Math.random()*10+1);
+var countplayer22=Math.floor(Math.random()*10+1);
+var total2=countplayer21+countplayer22
+console.log("Player two has" + total2);
+return total2
+}
+
+//function dealerHand() {
+var countdealer=Math.floor(Math.random()*10+1);
+var countdealer=Math.floor(Math.random()*10+1);
+var totaldealer=countdealer+countdealer
+console.log("Dealer has" + totaldealer);
+return totaldealer
+}
+
+
+//player1Hand()
+//player2Hand()
+//dealerHand()
+
+
+
+//if (player1Hand() > dealerHand()){
+console.log("Player One has" + player1Hand() +"is Beating the dealer" + dealerHand);}
+//else {console.log("Player 1 is losing to the dealer");
+}
+
+
+function suites() {
+         var finalhand= dealerhand()
+
+
+
+
+
+}
+*/
+
+
+
+
+
+// make a method in a function 
+
+
+//if (player1Hand() === dealerHand() ){
+//console.log("it's a push");}
+//else {
+//console.log ("on the right tack ");
+//}
+
+
+//var hit =prompt("Do you want to hit ?");
+ //var alpha = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+ 
+ //console.log
+ //adding a new git ocmment
+ 
+ function AddingComment(){
+ adding comment for second push }; 
+ 

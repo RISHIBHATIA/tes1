8be37479e4dc0bdfdf52e3702f2c42d210d0d614
Rishi Bhatia <RishiBhatia81@gmail.com>
2019-12-28 14:59:59 -0500

    adding the bookexamples.js

---
+++ bookexamples.js
@@ -0,0 +1,36 @@
+
+
+console.log("hello world");
+
+let dates = new Date();
+let a= dates.getFullYear();
+
+
+
+
+
+
+console.log(a);
+
+if (a < 2020) 
+console.log("Credit Card Expired");
+else 
+console.log("credit Card Valid"); 
+
+
+
+let books =["Tom Clamcy","Micheal Critchon","Peter Benchly", "Bret Eastton Ellis"];
+
+console.log(books[1]);
+
+books.push("Stephen King");
+
+
+console.log(books);
+
+   if (books.lenght < 2)
+   	console.log("Too many Authors");
+   else 
+   	console.log("fewer autthors" + books[3]);
+
+   //create objects for books authors and units sold conditionals who sold the most 
